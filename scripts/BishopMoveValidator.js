8571dc8589526c065c0e276ee2faeb08ec100eda
Raskolnikov <hristov.pavel88@gmail.com>
2017-06-18 17:24:47 +0300

    fixed bishop movement

--- scripts/BishopMoveValidator.js
+++ scripts/BishopMoveValidator.js
@@ -8,7 +8,45 @@ function validateBishopMove(field, piece) {
   let fieldIndex = letters.indexOf(fieldLetter);
   if ((fieldIndex - pieceIndex) === (fieldNumber - pieceNumber)
       || (pieceIndex - fieldIndex) === (fieldNumber - pieceNumber)) {
-        // TODO Bishop is not allowed to move through other pieces !!! 
+        // TODO Bishop is not allowed to move through other pieces !!!
+        if (fieldIndex > pieceIndex && fieldNumber > pieceNumber) {
+          let targetNum1 = fieldNumber - 1;
+          for (let i1 = fieldIndex - 1; i1 > pieceIndex; i1--) {
+            if (document.getElementById(letters[i1] + targetNum1).className.length > 0) {
+              return false;
+            }
+
+            targetNum1--;
+          }
+        } else if (pieceIndex > fieldIndex && fieldNumber > pieceNumber) {
+          let targetNum2 = pieceNumber + 1;
+          for (let i2 = pieceIndex - 1; i2 > fieldIndex; i2--) {
+            if (document.getElementById(letters[i2] + targetNum2).className.length > 0) {
+              return false;
+            }
+
+            targetNum2++;
+          }
+        } else if (fieldIndex > pieceIndex && pieceNumber > fieldNumber) {
+          let targetNum3 = fieldNumber + 1;
+          for (let i3 = fieldIndex - 1; i3 > pieceIndex; i3--) {
+            if (document.getElementById(letters[i3] + targetNum3).className.length > 0) {
+              return false;
+            }
+
+            targetNum3++;
+          }
+        } else if (pieceIndex > fieldIndex && pieceNumber > fieldNumber) {
+          let targetNum4 = pieceNumber - 1;
+          for (let i4 = pieceIndex - 1; i4 > fieldIndex; i4--) {
+            if (document.getElementById(letters[i4] + targetNum4).className.length > 0) {
+              return false;
+            }
+
+            targetNum4--;
+          }
+        }
+
 
       return true;
   }
