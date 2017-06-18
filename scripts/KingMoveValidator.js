2f328ebd926fc5182fc86fc0f8b1434993324b43
Raskolnikov <hristov.pavel88@gmail.com>
2017-06-18 21:05:12 +0300

    validation for king movement target field - in progress

--- scripts/KingMoveValidator.js
+++ scripts/KingMoveValidator.js
@@ -4,7 +4,7 @@ function validateKingMove(field, piece) {
   let pieceLetterIndex = letters.indexOf(piece.id[0]);
   let fieldNumber = Number(field.id[1]);
   let pieceNumber = Number(piece.id[1]);
-  // TODO Should add restriction for king move if the field is attacked by another piece!!! 
+  // TODO Should add restriction for king move if the field is attacked by another piece!!!
   if (fieldLetterIndex === pieceLetterIndex
     && (fieldNumber - pieceNumber === 1 || pieceNumber - fieldNumber === 1)) {
     return true;
@@ -20,6 +20,10 @@ function validateKingMove(field, piece) {
   }
 
   return false;
+}
+
+function validateIfTargetedMoveIsAttacked(fieldIndice, fieldNum) {
+  
 }
 
 export { validateKingMove };
