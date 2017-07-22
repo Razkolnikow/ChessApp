7c50888f5205cf79da6af310408282da0dff35ce
Raskolnikov <hristov.pavel88@gmail.com>
2017-07-22 11:37:21 +0300

    checkListener fix

--- scripts/CheckListener.js
+++ scripts/CheckListener.js
@@ -11,23 +11,29 @@ export class CheckListener {
     this._scanner = new KingAttackScanner();
   }
 
-  listen(field) { 
+  listen(field) {
     let fieldIndex = letters.indexOf(field.id[0]);
     let fieldNumber = Number(field.id[1]);
     let kingColor = this._scanner.getColor(field);
 
-    if (this._scanner.isAttackedFromRock(fieldIndex, fieldNumber, kingColor)) {
-      return true;
-    } else if (this._scanner.isAttackedFromPawn(fieldIndex, fieldNumber, kingColor)) {
-      return true;
-    } else if (this._scanner.isAttackedFromBishop(fieldIndex, fieldNumber, kingColor)) {
-      return true;
-    } else if (this._scanner.isAttackedFromKing(fieldIndex, fieldNumber, kingColor)) {
-      return true;
-    } else if (this._scanner.isAttackedFromKnight(fieldIndex, fieldNumber, kingColor)) {
-      return true;
-    } else if (this._scanner.isAttackedFromQueen(fieldIndex, fieldNumber, kingColor)) {
-      return true;
+    let rock = this._scanner.isAttackedFromRock(fieldIndex, fieldNumber, kingColor);
+    let pawn = this._scanner.isAttackedFromPawn(fieldIndex, fieldNumber, kingColor);
+    let bishop = this._scanner.isAttackedFromBishop(fieldIndex, fieldNumber, kingColor);
+    let king = this._scanner.isAttackedFromKing(fieldIndex, fieldNumber, kingColor);
+    let knight = this._scanner.isAttackedFromKnight(fieldIndex, fieldNumber, kingColor);
+    let queen = this._scanner.isAttackedFromQueen(fieldIndex, fieldNumber, kingColor);
+    if (rock) {
+      return rock;
+    } else if (pawn) {
+      return pawn;
+    } else if (bishop) {
+      return bishop;
+    } else if (king) {
+      return king;
+    } else if (knight) {
+      return knight;
+    } else if (queen) {
+      return queen;
     }
 
     return false;
